170164aa27fc6e6503ed5ef40cd810ba4f0895c2
Wingseter <wingseter@gmail.com>
2020-06-20 14:31:30 +0900

    Updates

---
+++ precache-manifest.180f2c1a5d2c4a6cb6537b38e60ee544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c518c8a376ca4f87a7c0f5daa5a64f",
+    "url": "/movie_ration_app/index.html"
+  },
+  {
+    "revision": "5c99cd20c71178fcb34e",
+    "url": "/movie_ration_app/static/css/main.34b4798f.chunk.css"
+  },
+  {
+    "revision": "2fa08fbf60ba1820b956",
+    "url": "/movie_ration_app/static/js/2.ddcff673.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_ration_app/static/js/2.ddcff673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c99cd20c71178fcb34e",
+    "url": "/movie_ration_app/static/js/main.cf400621.chunk.js"
+  },
+  {
+    "revision": "5e22495662547b706bfd",
+    "url": "/movie_ration_app/static/js/runtime-main.cff48a3f.js"
+  }
+]);
