490aaad8689f7b55c258827a2b20c8ac1a2e1b71
Wingseter <wingseter@gmail.com>
2020-06-21 18:39:28 +0900

    add Router and Navigation

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css"
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>About this page: You can see Movie Information</span>
+            <span>-by wingseter-</span>
+        </div>
+    )
+}
+
+export default About;
